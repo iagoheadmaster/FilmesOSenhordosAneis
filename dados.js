fe7b1d6583f84f8cf6ca85eb96109d9067dd7282
Iago Menezes Soares <167209965+iagoheadmaster@users.noreply.github.com>
2024-09-08 14:35:51 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,33 +1,40 @@
 let dados = [
+   // Trilogia O Senhor dos Aneis
   {
     titulo: "O Senhor dos Anéis: A Sociedade do Anel",
     sinopse: "Um hobbit chamado Frodo Bolseiro encontra um anel mágico que pode dominar o mundo. Acompanhado por uma irmandade, ele embarca em uma jornada épica para destruir o anel na Montanha da Perdição.",
-    link: "https://pt.wikipedia.org/wiki/o_senhor_dos_aneis_a_sociedade_do_anel"
+    link: "https://pt.wikipedia.org/wiki/o_senhor_dos_aneis_a_sociedade_do_anel",
+    tags: "frodo aneis mordor magico epica mordor hobbits hobbitses sam samwise gamgee gandalf aragorn legolas gimli boromir meriadoc brandybuck Merry peregrin Took Pippin"
   },
   {
     titulo: "O Senhor dos Anéis: As Duas Torres",
     sinopse: "A Sociedade do Anel é separada e Frodo e Sam continuam sua jornada para Mordor, enquanto Aragorn, Legolas e Gimli buscam os hobbits capturados. As forças de Saruman atacam Rohan.",
-    link: "https://pt.wikipedia.org/wiki/As_Duas_Torres"
+    link: "https://pt.wikipedia.org/wiki/As_Duas_Torres",
+    tags: "rohan gondor orcs ents isengard merry pippin aragorn legolas gimli guerra arvore arvores árvore árvores uruk rai uruk hai"
   },
   {
     titulo: "O Senhor dos Anéis: O Retorno do Rei",
     sinopse: "A batalha final por Mordor se aproxima. Frodo e Sam se infiltram em Mordor, enquanto um grande exército se reúne para atacar a fortaleza de Barad-dûr.",
-    link: "https://pt.wikipedia.org/wiki/o_senhor_dos_aneis_o_retorno_do_rei"
+    link: "https://pt.wikipedia.org/wiki/o_senhor_dos_aneis_o_retorno_do_rei",
+    tags: "aneis batalha barad-dur barad dur bara dûr sauron faramir eowyn gondor rohan espada andúril anduril exercito"
   },
   // Trilogia O Hobbit
   {
     titulo: "O Hobbit: Uma Jornada Inesperada",
     sinopse: "Bilbo Bolseiro, um hobbit pacífico, é recrutado por um mago e um grupo de anões para recuperar o reino de Erebor, tomado pelo dragão Smaug.",
-    link: "https://pt.wikipedia.org/wiki/o_hobbit_uma_jornada_inesperada"
+    link: "https://pt.wikipedia.org/wiki/o_hobbit_uma_jornada_inesperada",
+    tags: "aneis anoes dragao aventura tesouro"
   },
   {
     titulo: "O Hobbit: A Desolação de Smaug",
     sinopse: "Bilbo e a companhia continuam sua jornada, enfrentando perigos e chegando à Montanha Solitária, onde encontram o poderoso dragão Smaug.",
-    link: "https://pt.wikipedia.org/wiki/o_hobbit_a_desolação_de_smaug"
+    link: "https://pt.wikipedia.org/wiki/o_hobbit_a_desolação_de_smaug",
+    tags: "aneis dragao desolacao desolaçao"
   },
   {
     titulo: "O Hobbit: A Batalha dos Cinco Exércitos",
     sinopse: "A batalha final por Erebor se aproxima, reunindo elfos, homens e anões em um conflito épico. Em meio ao caos, o Anel Único revela seu poder, e Bilbo toma uma decisão que mudará o curso da história.",
-    link: "https://pt.wikipedia.org/wiki/o_hobbit_a_batalha_dos_cinco_exercitos"
+    link: "https://pt.wikipedia.org/wiki/o_hobbit_a_batalha_dos_cinco_exercitos",
+    tags: "aneis exercito exercitos exército anoes um anel anéis"
   },
 ];
