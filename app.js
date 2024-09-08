4e3888158d6f3442c8067158e72f095a204b2a5c
Iago Menezes Soares <167209965+iagoheadmaster@users.noreply.github.com>
2024-09-08 14:35:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,31 @@ function pesquisar() {
     // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    //se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "Nada foi pesquisado. Busque algo no campo de pesquisa."
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
     // Inicializa uma string vazia para armazenar os resultados da pesquisa
     let resultados = "";
+    let titulo = "";
+    let sinopse = "";
+    let tags = "";
 
     // Itera sobre cada dado na lista de dados
     for (let dado of dados) {
-        // Concatena HTML para criar um novo elemento de resultado
-        resultados += `
+        titulo = dado.titulo.toLowerCase()
+        sinopse = dado.sinopse.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        // se titulo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || sinopse.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // cria um novo elemento
+            resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a>
@@ -17,8 +35,13 @@ function pesquisar() {
                 <a href=${dado.link} target="_blank">Mais informações</a>
             </div>
         `;
+        }
+        // Concatena HTML para criar um novo elemento de resultado
     }
 
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
+    }
     // Atribui a string com os resultados ao conteúdo HTML da seção
     section.innerHTML = resultados;
 }
