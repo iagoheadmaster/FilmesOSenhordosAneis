04acb2be5f31042706f7d39ab74ced15dac30c47
Iago Menezes Soares <167209965+iagoheadmaster@users.noreply.github.com>
2024-09-09 12:22:45 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -3,38 +3,38 @@ let dados = [
   {
     titulo: "O Senhor dos Anéis: A Sociedade do Anel",
     sinopse: "Um hobbit chamado Frodo Bolseiro encontra um anel mágico que pode dominar o mundo. Acompanhado por uma irmandade, ele embarca em uma jornada épica para destruir o anel na Montanha da Perdição.",
-    link: "https://pt.wikipedia.org/wiki/o_senhor_dos_aneis_a_sociedade_do_anel",
+    link: "https://www.primevideo.com/detail/0N0CRBLY1S5GDA4EVTQ34LF5GN/ref=atv_sr_fle_c_Tn74RA_5_1_5?sr=1-5&pageTypeIdSource=ASIN&pageTypeId=B086DC34LZ&qid=1725895093210",
     tags: "frodo aneis mordor magico epica mordor hobbits hobbitses sam samwise gamgee gandalf aragorn legolas gimli boromir meriadoc brandybuck Merry peregrin Took Pippin"
   },
   {
     titulo: "O Senhor dos Anéis: As Duas Torres",
     sinopse: "A Sociedade do Anel é separada e Frodo e Sam continuam sua jornada para Mordor, enquanto Aragorn, Legolas e Gimli buscam os hobbits capturados. As forças de Saruman atacam Rohan.",
-    link: "https://pt.wikipedia.org/wiki/As_Duas_Torres",
+    link: "https://www.primevideo.com/detail/0OSAJRJAW5BHY843QHALJZFUTX/ref=atv_sr_fle_c_Tn74RA_3_1_3?sr=1-3&pageTypeIdSource=ASIN&pageTypeId=B08669L8N7&qid=1725895130004",
     tags: "rohan gondor orcs ents isengard merry pippin aragorn legolas gimli guerra arvore arvores árvore árvores uruk rai uruk hai"
   },
   {
     titulo: "O Senhor dos Anéis: O Retorno do Rei",
     sinopse: "A batalha final por Mordor se aproxima. Frodo e Sam se infiltram em Mordor, enquanto um grande exército se reúne para atacar a fortaleza de Barad-dûr.",
-    link: "https://pt.wikipedia.org/wiki/o_senhor_dos_aneis_o_retorno_do_rei",
+    link: "https://www.primevideo.com/detail/0NSGBFYAQDZQKEC8BBWKT8GILJ/ref=atv_sr_fle_c_Tn74RA_6_1_6?sr=1-6&pageTypeIdSource=ASIN&pageTypeId=B09HVDT96J&qid=1725895093210",
     tags: "aneis batalha barad-dur barad dur bara dûr sauron faramir eowyn gondor rohan espada andúril anduril exercito"
   },
   // Trilogia O Hobbit
   {
     titulo: "O Hobbit: Uma Jornada Inesperada",
     sinopse: "Bilbo Bolseiro, um hobbit pacífico, é recrutado por um mago e um grupo de anões para recuperar o reino de Erebor, tomado pelo dragão Smaug.",
-    link: "https://pt.wikipedia.org/wiki/o_hobbit_uma_jornada_inesperada",
+    link: "https://www.primevideo.com/detail/0RZDRRR46MYHPSQE8MNH0HQKZ8/ref=atv_sr_fle_c_Tn74RA_1_1_1?sr=1-1&pageTypeIdSource=ASIN&pageTypeId=B0CRJVKPL6&qid=1725895093210",
     tags: "aneis anoes dragao aventura tesouro"
   },
   {
     titulo: "O Hobbit: A Desolação de Smaug",
     sinopse: "Bilbo e a companhia continuam sua jornada, enfrentando perigos e chegando à Montanha Solitária, onde encontram o poderoso dragão Smaug.",
-    link: "https://pt.wikipedia.org/wiki/o_hobbit_a_desolação_de_smaug",
+    link: "https://www.primevideo.com/detail/0OWK01NX6K76UK4PIK6J6Y9QES/ref=atv_sr_fle_c_Tn74RA_2_1_2?sr=1-2&pageTypeIdSource=ASIN&pageTypeId=B08NCX9FP7&qid=1725895093210",
     tags: "aneis dragao desolacao desolaçao"
   },
   {
     titulo: "O Hobbit: A Batalha dos Cinco Exércitos",
     sinopse: "A batalha final por Erebor se aproxima, reunindo elfos, homens e anões em um conflito épico. Em meio ao caos, o Anel Único revela seu poder, e Bilbo toma uma decisão que mudará o curso da história.",
-    link: "https://pt.wikipedia.org/wiki/o_hobbit_a_batalha_dos_cinco_exercitos",
+    link: "https://www.primevideo.com/detail/0IE43RNF7538E9TCNH6NAXCMZP/ref=atv_sr_fle_c_Tn74RA_3_1_3?sr=1-3&pageTypeIdSource=ASIN&pageTypeId=B0855N1C3M&qid=1725895093210",
     tags: "aneis exercito exercitos exército anoes um anel anéis"
   },
 ];
